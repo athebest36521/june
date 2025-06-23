fcc0729e5dd54b49812161b6c8f001fa94a5dd81
athebest36521 <526atiwari@frhsd.com>
2025-06-23 12:01:13 -0400

    Update script.js
    trying again

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <p class="card-text">
                       ${p.price}
                     </p>
-                   <button class="btn btn-primary" onClick="topItems"('${p.name}-${p.price}', 'topItems')">More Info</button>
+                   <button class="btn btn-primary" onClick="topItems"('${p.name}-${p.price}', 'featureProducts)">More Info</button>
                
                <div id='${p.name}-${p.price}' style="display:none">
                
